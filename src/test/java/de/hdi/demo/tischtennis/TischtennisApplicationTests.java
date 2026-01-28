4771a51617756152b04389e76172e1e3cb5bb8ce
Paul Salomon <paul.salomon@hdi.de>
2026-01-28 15:44:46 +0100

    start

---
+++ src/test/java/de/hdi/demo/tischtennis/TischtennisApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hdi.demo.tischtennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TischtennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
